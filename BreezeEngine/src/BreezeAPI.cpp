ea205e7494ca277f07833e17cf0198d192c4e5c7
Mystic Quest <l1skou@gmail.com>
2023-06-10 20:25:34 +0300

    makes api more readable

--- BreezeEngine/src/BreezeAPI.cpp
+++ BreezeEngine/src/BreezeAPI.cpp
@@ -1,29 +1,19 @@
-#include "BreezeAPI.h"
 #include "SettingsResolver.h"
 #include "MySDLWindow.h"
 #include "GameCore.h"
-#include <memory>
-
-class BreezeAPIImpl
-{
-public:
-    std::unique_ptr<SettingsResolver> settings;
-    std::unique_ptr<MySDLWindow> window;
-    std::unique_ptr<GameCore> game;
-};
+#include "BreezeAPI.h"
 
 BreezeAPI::BreezeAPI(const std::string& settingsFile, const char* title)
-    : pImplementation(std::make_unique<BreezeAPIImpl>()) // Initialize the pImpl
 {
-    pImplementation->settings = std::make_unique<SettingsResolver>();
-    Config config = pImplementation->settings->ReadSettings(settingsFile);
-    pImplementation->window = std::make_unique<MySDLWindow>(title, config.width, config.height);
-    pImplementation->game = std::make_unique<GameCore>(*pImplementation->window, config.frame_rate);
+    settings = std::make_unique<SettingsResolver>();
+    Config config = settings->ReadSettings(settingsFile);
+    window = std::make_unique<MySDLWindow>(title, config.width, config.height);
+    game = std::make_unique<GameCore>(*window, config.frame_rate);
 }
 
 BreezeAPI::~BreezeAPI() = default;
 
 void BreezeAPI::Run()
 {
-    pImplementation->game->Run(*pImplementation->window);
+    game->Run(*window);
 }
--- BreezeEngine/src/BreezeAPI.h
+++ BreezeEngine/src/BreezeAPI.h
@@ -1,18 +1,20 @@
-#include "Export.h"
-#include <string>
 #include <memory>
+#include <iostream>
+#include "Export.h"
 
-class BreezeAPIImpl; // forward declaration
+class SettingsResolver;
+class MySDLWindow;
+class GameCore;
 
 class BREEZEENGINE_API BreezeAPI
 {
 public:
     BreezeAPI(const std::string& settingsFile, const char* title);
     ~BreezeAPI();
-
     void Run();
 
 private:
-    std::unique_ptr<BreezeAPIImpl> pImplementation;
+    std::unique_ptr<SettingsResolver> settings;
+    std::unique_ptr<MySDLWindow> window;
+    std::unique_ptr<GameCore> game;
 };
-
