1d9e53cffb32ec63033b488765b82ed9b6168b9b
Mystic Quest <l1skou@gmail.com>
2023-06-24 21:11:39 +0300

    minor input fix

--- BreezeEngine/src/ECS/Systems/InputSystem.cpp
+++ BreezeEngine/src/ECS/Systems/InputSystem.cpp
@@ -9,14 +9,11 @@ void InputSystem::Update(ECSManager& ecs)
         if (ecs.transformComponents.contains(e) && ecs.inputComponents.contains(e))
         {
 
-            if (keys[SDL_SCANCODE_A]) { ecs.transformComponents[e].position.X = -1.0f;
+            if (keys[SDL_SCANCODE_A]) { ecs.transformComponents[e].position.X -= 1.0f;
             }
-            if (keys[SDL_SCANCODE_S]) { ecs.transformComponents[e].position.Y = 1.0f; }
-            if (keys[SDL_SCANCODE_W]) { ecs.transformComponents[e].position.Y = -1.0f; }
-            if (keys[SDL_SCANCODE_D]) { ecs.transformComponents[e].position.X = 1.0f; }
-
-            if (!keys[SDL_SCANCODE_A] && !keys[SDL_SCANCODE_D]) { ecs.transformComponents[e].position.X = 0.0f; }
-            if (!keys[SDL_SCANCODE_S] && !keys[SDL_SCANCODE_W]) { ecs.transformComponents[e].position.Y = 0.0f; }
+            if (keys[SDL_SCANCODE_S]) { ecs.transformComponents[e].position.Y += 1.0f; }
+            if (keys[SDL_SCANCODE_W]) { ecs.transformComponents[e].position.Y -= 1.0f; }
+            if (keys[SDL_SCANCODE_D]) { ecs.transformComponents[e].position.X += 1.0f; }
         }
     }
 }
