247b28fb47a1ebb1068578c1e308b58b2bec63e5
Mystic Quest <kirimns@gmail.com>
2023-06-30 01:51:39 +0300

    adds collision system implementation - to do: sdl_frect

---
+++ BreezeEngine/src/ECS/Systems/CollisionSystem.cpp
@@ -0,0 +1,43 @@
+#include "CollisionSystem.h"
+
+void CollisionSystem::Update(ECSManager& ecs)
+{
+    for (int e1 = 1; e1 <= max_entity; e1++)
+    {
+        if (ecs.collisionComponents.contains(e1) && ecs.renderComponents.contains(e1))
+        {
+
+            // Sync position and size from RenderComponent to CollisionComponent
+            const SDL_Rect& dstRect = ecs.renderComponents[e1].dstRect;
+            ecs.collisionComponents[e1].aabb = { dstRect.x, dstRect.y, dstRect.w, dstRect.h };
+
+            for (int e2 = e1 + 1; e2 <= max_entity; e2++)
+            {
+                if (ecs.collisionComponents.contains(e2) && ecs.renderComponents.contains(e2))
+                {
+
+                    // Sync position and size from RenderComponent to CollisionComponent
+                    const SDL_Rect& dstRect2 = ecs.renderComponents[e2].dstRect;
+                    ecs.collisionComponents[e2].aabb = { dstRect2.x, dstRect2.y, dstRect2.w, dstRect2.h };
+
+                    const AABB& box1 = ecs.collisionComponents[e1].aabb;
+                    const AABB& box2 = ecs.collisionComponents[e2].aabb;
+
+                    if (AABBcollision(box1, box2))
+                    {
+                        // Collision detected between entity e1 and entity e2
+                        // Handle collision
+                    }
+                }
+            }
+        }
+    }
+}
+
+bool CollisionSystem::AABBcollision(const AABB& a, const AABB& b)
+{
+    return (a.x < (b.x + b.width) &&
+        (a.x + a.width) > b.x &&
+        a.y < (b.y + b.height) &&
+        (a.y + a.height) > b.y);
+}
