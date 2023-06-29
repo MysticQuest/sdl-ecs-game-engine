3e04752c4caf1b29340257ba31ce22a39ae7591a
Mystic Quest <kirimns@gmail.com>
2023-06-30 01:23:57 +0300

    separates ecsmanager

--- BreezeEngine/src/ECS/ECSManager.cpp
+++ BreezeEngine/src/ECS/ECSManager.cpp
@@ -1,1 +1,89 @@
-//#include "ECSManager.h"
+#include "ECSManager.h"
+
+template<typename T>
+void ECSManager::AddComponent(Entity entity, T component)
+{
+    static_assert(
+        std::is_same<T, RenderComponent>::value ||
+        std::is_same<T, TransformComponent>::value ||
+        std::is_same<T, InputComponent>::value ||
+        std::is_same<T, CollisionComponent>::value,
+        "Invalid component type."
+        );
+
+    GetComponentMap<T>()[entity] = component;
+}
+
+template<typename T>
+void ECSManager::RemoveComponent(Entity entity)
+{
+    static_assert(
+        std::is_same<T, RenderComponent>::value ||
+        std::is_same<T, TransformComponent>::value ||
+        std::is_same<T, InputComponent>::value ||
+        std::is_same<T, CollisionComponent>::value,
+        "Invalid component type."
+        );
+
+    GetComponentMap<T>().erase(entity);
+}
+
+template<typename T>
+T& ECSManager::GetComponent(Entity entity)
+{
+    static_assert(
+        std::is_same<T, RenderComponent>::value ||
+        std::is_same<T, TransformComponent>::value ||
+        std::is_same<T, InputComponent>::value ||
+        std::is_same<T, CollisionComponent>::value,
+        "Invalid component type."
+        );
+
+    return GetComponentMap<T>()[entity];
+}
+
+template<typename T>
+std::unordered_map<Entity, T>& ECSManager::GetComponentMap()
+{
+    static_assert(sizeof(T) == 0, "Unsupported component type.");
+}
+
+template<>
+std::unordered_map<Entity, RenderComponent>& ECSManager::GetComponentMap<RenderComponent>()
+{
+    return renderComponents;
+}
+
+template<>
+std::unordered_map<Entity, TransformComponent>& ECSManager::GetComponentMap<TransformComponent>()
+{
+    return transformComponents;
+}
+
+template<>
+std::unordered_map<Entity, InputComponent>& ECSManager::GetComponentMap<InputComponent>()
+{
+    return inputComponents;
+}
+
+template<>
+std::unordered_map<Entity, CollisionComponent>& ECSManager::GetComponentMap<CollisionComponent>()
+{
+    return collisionComponents;
+}
+
+// Explicit template instantiation
+template void ECSManager::AddComponent<RenderComponent>(Entity, RenderComponent);
+template void ECSManager::AddComponent<TransformComponent>(Entity, TransformComponent);
+template void ECSManager::AddComponent<InputComponent>(Entity, InputComponent);
+template void ECSManager::AddComponent<CollisionComponent>(Entity, CollisionComponent);
+
+template void ECSManager::RemoveComponent<RenderComponent>(Entity);
+template void ECSManager::RemoveComponent<TransformComponent>(Entity);
+template void ECSManager::RemoveComponent<InputComponent>(Entity);
+template void ECSManager::RemoveComponent<CollisionComponent>(Entity);
+
+template RenderComponent& ECSManager::GetComponent<RenderComponent>(Entity);
+template TransformComponent& ECSManager::GetComponent<TransformComponent>(Entity);
+template InputComponent& ECSManager::GetComponent<InputComponent>(Entity);
+template CollisionComponent& ECSManager::GetComponent<CollisionComponent>(Entity);
--- BreezeEngine/src/ECS/ECSManager.h
+++ BreezeEngine/src/ECS/ECSManager.h
@@ -1,6 +1,5 @@
 #pragma once
 
-#include <vector>
 #include <unordered_map>
 #include "Entity.h"
 #include "Components/RenderComponent.h"
@@ -12,46 +11,13 @@ class ECSManager
 {
 public:
     template<typename T>
-    void AddComponent(Entity entity, T component)
-    {
-        static_assert(
-            std::is_same<T, RenderComponent>::value ||
-            std::is_same<T, TransformComponent>::value ||
-            std::is_same<T, InputComponent>::value ||
-            std::is_same<T, CollisionComponent>::value,
-            "Invalid component type."
-            );
-
-        GetComponentMap<T>()[entity] = component;
-    }
+    void AddComponent(Entity entity, T component);
 
     template<typename T>
-    void RemoveComponent(Entity entity)
-    {
-        static_assert(
-            std::is_same<T, RenderComponent>::value ||
-            std::is_same<T, TransformComponent>::value ||
-            std::is_same<T, InputComponent>::value ||
-            std::is_same<T, CollisionComponent>::value,
-            "Invalid component type."
-            );
-
-        GetComponentMap<T>().erase(entity);
-    }
+    void RemoveComponent(Entity entity);
 
     template<typename T>
-    T& GetComponent(Entity entity)
-    {
-        static_assert(
-            std::is_same<T, RenderComponent>::value ||
-            std::is_same<T, TransformComponent>::value ||
-            std::is_same<T, InputComponent>::value ||
-            std::is_same<T, CollisionComponent>::value,
-            "Invalid component type."
-            );
-
-        return GetComponentMap<T>()[entity];
-    }
+    T& GetComponent(Entity entity);
 
     std::unordered_map<Entity, RenderComponent> renderComponents;
     std::unordered_map<Entity, TransformComponent> transformComponents;
@@ -60,32 +26,5 @@ public:
 
 private:
     template<typename T>
-    std::unordered_map<Entity, T>& GetComponentMap()
-    {
-        static_assert(sizeof(T) == 0, "Unsupported component type.");
-    }
-
-    template<>
-    std::unordered_map<Entity, RenderComponent>& GetComponentMap<RenderComponent>()
-    {
-        return renderComponents;
-    }
-
-    template<>
-    std::unordered_map<Entity, TransformComponent>& GetComponentMap<TransformComponent>()
-    {
-        return transformComponents;
-    }
-
-    template<>
-    std::unordered_map<Entity, InputComponent>& GetComponentMap<InputComponent>()
-    {
-        return inputComponents;
-    }
-
-    template<>
-    std::unordered_map<Entity, CollisionComponent>& GetComponentMap<CollisionComponent>()
-    {
-        return collisionComponents;
-    }
+    std::unordered_map<Entity, T>& GetComponentMap();
 };
