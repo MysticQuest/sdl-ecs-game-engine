895d36f2821de7ed16a0528147e7b1b69ffff6e8
Mystic Quest <kirimns@gmail.com>
2023-10-19 00:13:52 +0300

    minor warning fix

--- Game0/src/World.cpp
+++ Game0/src/World.cpp
@@ -22,13 +22,13 @@ void World::Init()
 	std::wstring message1 = L"Added Entity with id: " + std::to_wstring(entity1) + L'\n'; 
 	OutputDebugString(message1.c_str()); 
 	breezeAPI.AddRenderer(entity1, "res/textures/pac1.png");
-	breezeAPI.AddTranform(entity1, Vector2f(300, 300), Vector2f(.1, .1), 90, Vector2f(.5, .3f)); 
+	breezeAPI.AddTranform(entity1, Vector2f(300, 300), Vector2f(.1f, .1f), 90, Vector2f(.5f, .3f)); 
 
 	Entity entity2 = breezeAPI.AddEntity();
 	std::wstring message2 = L"Added Entity with id: " + std::to_wstring(entity2) + L'\n';
 	OutputDebugString(message2.c_str());
 	breezeAPI.AddRenderer(entity2, "res/textures/pac1.png");
-	breezeAPI.AddTranform(entity2, Vector2f(0, 500), Vector2f(.1, -.1), 90, Vector2f(.5, .3f));
+	breezeAPI.AddTranform(entity2, Vector2f(0, 500), Vector2f(.1f, -.1f), 90, Vector2f(.5f, .3f));
 }
 
 void World::Update()
